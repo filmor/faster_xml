901aa08700d8f1523f70fdcf701d3a1bd0c8add1
Benedikt Reinartz <filmor@gmail.com>
2019-10-04 18:23:49 +0200

    Initial commit

---
+++ crates/faster_xml_nif/src/lib.rs
@@ -0,0 +1,58 @@
+use std::io::Cursor;
+
+use rustler;
+use rustler::{Encoder, Env, NifResult, Term, Error};
+use rustler::types::{Binary, Pid};
+
+use quick_xml::Reader;
+use quick_xml::events::Event;
+
+mod atoms {
+    rustler::rustler_atoms! {
+        atom ok;
+        atom start;
+        atom end;
+        atom empty;
+    }
+}
+
+rustler::rustler_export_nifs! {
+    "faster_xml",
+    [
+        ("parse", 2, parse),
+    ],
+    Some(on_load)
+}
+
+pub fn on_load<'a>(_env: Env<'a>, _load_info: Term<'a>) -> bool {
+    true
+}
+
+fn parse<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
+    let pid: Pid = args[0].decode()?;
+    let data: Binary = args[1].decode()?;
+
+    let reader = Cursor::new(data.as_slice());
+    let mut reader = Reader::from_reader(reader);
+
+    let mut buf = Vec::new();
+
+    loop {
+        match reader.read_event(&mut buf) {
+            Ok(Event::Start(ref t)) => {
+                env.send(&pid, (atoms::start(), t.name()).encode(env))
+            },
+            Ok(Event::End(ref t)) => {
+                env.send(&pid, (atoms::end(), t.name()).encode(env))
+            },
+            Ok(Event::Empty(ref t)) => {
+                env.send(&pid, (atoms::empty(), t.name()).encode(env))
+            }
+            Ok(Event::Eof) => break,
+            Ok(_) => (),
+            Err(_) => return Err(Error::Atom("broken")),
+        }
+    }
+
+    Ok((atoms::ok(), count).encode(env))
+}
---
+++ src/faster_xml.erl
@@ -0,0 +1,23 @@
+-module(faster_xml).
+
+-export([
+    parse/1,
+    parse_file/1
+]).
+
+-on_load(init/0).
+
+init() ->
+    {ok, Lib} = find_crate:find_library(?MODULE, "faster_xml_nif"),
+    erlang:load_nif(Lib, 0).
+
+
+parse(Bin) ->
+    parse(self(), Bin).
+
+parse(_Pid, _Bin) ->
+    erlang:nif_error(nif_not_loaded).
+
+parse_file(FName) ->
+    {ok, Bin} = file:read_file(FName),
+    parse(Bin).
